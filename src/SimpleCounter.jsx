cd24d334111dc04f0b3981a7747c8884747588e8
Heather Amler <amler.heather@gmail.com>
2023-12-04 21:59:46 -0500

    Check that second arg/dependency for useEffect only runs for specified state

--- src/SimpleCounter.jsx
+++ src/SimpleCounter.jsx
@@ -2,16 +2,23 @@ import { useState, useEffect } from "react";
 
 export default function SimpleCounter() {
     const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
     useEffect(function myEffect() {
-        console.log('myEffect called');
-    });
+            console.log('myEffect called');
+        }, 
+        [count]
+    );
     const increment = () => {
         setCount(c => c + 1);
+    };
+    const handleChange = (e) => {
+        setName(e.target.value);
     };
     return (
         <div>
             <h1>Count: {count}</h1>
             <button onClick={increment}>+1</button>
+            <input onChange={handleChange} type="text" value={name} />
         </div>
     );
 }
