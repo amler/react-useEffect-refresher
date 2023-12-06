76f9c25f1538aa4641844dba7831fc81011c166e
Heather Amler <amler.heather@gmail.com>
2023-12-06 14:49:22 -0500

    Getting data via fetch on mount

--- src/SwansonQuote.jsx
+++ src/SwansonQuote.jsx
@@ -1,17 +1,28 @@
-
+import { useState, useEffect } from "react";
 const JSON_API_APP = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
 
 export default function SwansonQuote() {
-
+    const [quote , setQuote] = useState('');
+    useEffect(() => {
+        fetchQuote();
+    } ,[]);
+    async function fetchQuote() {
+        const response = await fetch(JSON_API_APP);
+        const [jsonResponse] = await response.json();
+        setQuote(jsonResponse);
+    }
 
     return (
-        <figure>
-            <blockquote>
-                TBD
-            </blockquote>
-            <figcaption>
-                Ron Swanson, <cite>Parks & Rec</cite>
-            </figcaption>
-        </figure>
+        <>
+            <figure>
+                <blockquote>
+                    {quote}
+                </blockquote>
+                <figcaption>
+                    Ron Swanson, <cite>Parks & Rec</cite>
+                </figcaption>
+            </figure>
+            <button onClick={fetchQuote}>More Ron Wisdom</button>
+        </>
     );
 }
